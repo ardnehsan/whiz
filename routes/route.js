c32395ec05e2d7ab0929781e91bdd2f1a274a84d
BradHemmerick <Brad.hemmerick13@gmail.com>
2018-01-24 14:48:28 -0600

    more clean up

--- routes/route.js
+++ routes/route.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-// Search Store Page
-router.get('/', ensureAuthenticated, function (req, res, next) {
-	res.render('index');
-});
+// // Search Store Page
+// router.get('/', ensureAuthenticated, function (req, res, next) {
+// 	res.render('index');
+// });
 
-function ensureAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect('/users/login');
-}
+// function ensureAuthenticated(req, res, next) {
+// 	if (req.isAuthenticated()) {
+// 		return next();
+// 	}
+// 	res.redirect('/users/login');
+// }
 
-module.exports = router;
+// module.exports = router;
